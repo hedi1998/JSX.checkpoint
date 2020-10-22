19f1b05654fe0b3e1130d8a1d0ccc2d421f30200
hedi1998 <magrounhedi8@gmail.com>
2020-10-22 14:59:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import secondImage from './images.jfif'
+function App() {
+  return (
+    <div>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Your name here</h1>
+      <br />
+      <img src="/images (1).jfif" />
+      <br />
+      <img src={secondImage} />
+    </div>
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>)}
+export default App;
